82005fb2f2c085eadfbe3a28d30546db08c36a2b
Miquel Garcia <miquel.garcia@rokubun.cat>
2023-10-06 08:14:35 +0000

    feat(source): add helper to compute dec hours

--- source/helpers.py
+++ source/helpers.py
@@ -6,5 +6,8 @@ import pandas as pd
 def compute_elapsed_seconds(epochs:pd.Series) -> pd.Series:
     return (epochs - epochs.iloc[0]).dt.total_seconds()
 
+def compute_decimal_hours(epochs:pd.Series) -> pd.Series:
+    return epochs.apply(lambda x: x.hour + x.minute / 60 + x.second / 3600)
+
 def compute_rms(values:Iterable) -> float:
     return np.sqrt(np.mean(np.square(values)))
